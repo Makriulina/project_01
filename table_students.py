a8b2fe82a8833636eb25dc754dd3b53a00e8dc0a
Makriulina <makriulina@bk.ru>
2023-05-31 00:54:00 +1000

    file table_students was changed

--- table_students.py
+++ table_students.py
@@ -1,10 +1,11 @@
 
 
+
 import sqlite3
 
 connection = sqlite3.connect('teachers.db')
 cursor = connection.cursor()
-sqlquery = """CREATE TABLE Studentsschool(
+sqlquery = """CREATE TABLE School(
 Student_Id INTEGER NOT NULL PRIMARY KEY, 
 Student_Name TEXT NOT NULL, 
 School_Id INTEGER NOT NULL
@@ -19,7 +20,7 @@ import sqlite3
 
 connection = sqlite3.connect('teachers.db')
 cursor = connection.cursor()
-sqlquery = """INSERT INTO Studentsschool(Student_Id, Student_Name, School_Id) 
+sqlquery = """INSERT INTO School(Student_Id, Student_Name, School_Id) 
 VALUES 
 ('201', 'Иван', 1),
 ('202', 'Петр', 2),
@@ -31,48 +32,15 @@ connection.close()
 
 import sqlite3
 
-def get_connection():
-  connection = sqlite3.connect('teachers.db')
-  return connection
-  def close_connection(connection):
-    
-    if connection:
-      connection.close()
-def database_version():
-        try:
-          connection = get_connection()
-          cursor = connection.cursor()
-          cursor.execute("SELECT sqlite_version();")
-          db_version = cursor.fetchone()
-          print("Вы подключились к SQLite версии: ", db_version)
-          close_connection(connection)
-        except(Exception, sqlite3.Error) as error:
-          print("Ошибка в получении данных: ", error)
-          database_version()
+def get_student(student_id):
 
+  connection = sqlite3.connect("teachers.db")
+  cursor = connection.cursor()
+  query = "SELECT * FROM School JOIN Students ON School.School_Id = Students.School_Id WHERE Students.Student_id = ?"
+  cursor.execute(query,(student_id,))
+  records = cursor.fetchall()
+  print(records)
+  for row in records:
+    print()
 
-
-
-import sqlite3
-def get_connection():
-    connection = sqlite3.connect('teatcher.db')
-    return connection
-
-def close_connection(connection):
-    if connection:
-        connection.close()
-def get_student_detail(Student_Id):
-  
-    try:
-
-                connection = get_connection()
-                cursor = connection.cursor()
-                select_query = "SELECT * FROM Studentsschool WHERE Student_Id = ?"
-                cursor.execute(select_query,(Student_Id,))
-                records = cursor.fetchall()
-                print(records)        
-    except(Exception, sqlite3.Error) as error:
-                print("Ошибка в получении данных: ", error)
-                get_student_detail(201)
-
-
+get_student(201)
